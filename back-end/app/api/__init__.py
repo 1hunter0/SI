48177aa9de1fb83a0de61724e770ef94902185d1
1hunter0 <1292250315@qq.com>
2022-09-12 17:38:46 +0800

    revise the architecture

---
+++ back-end/app/api/__init__.py
@@ -0,0 +1,31 @@
+from app.api.api_ip import router_ip
+
+# router = APIRouter()
+
+# @router.get("/url/{url}")
+# async def getURL(url):
+#     return {}
+
+
+# @router.get("/ip/{ip}")
+# async def getIP(ip):
+#     return {}
+
+
+# @router.get("/dns/{dns}")
+# async def getDNS(ip):
+#     return {}
+
+
+# @router.get("/file/{file}")
+# async def getFile(file):
+#     return {}
+
+
+# @router.get("/graph/open/{name}")
+# async def getOpenInformation(file):
+#     return {}
+
+# @router.get("/graph/innear/{name}")
+# async def getinnearInformation(file):
+#     return {}
--- back-end/main.py
+++ back-end/main.py
@@ -4,15 +4,12 @@ from app.database import Base, engine
 from app.api import router_ip
 
 Base.metadata.create_all(bind=engine)
-from api.api import router
 
 app = FastAPI()
 
 app.include_router(router_ip)
 
-app.include_router(router)
 
-app.include_router(router)
 
 if __name__ == '__main__':
     uvicorn.run(app, host="127.0.0.1", port=8000)

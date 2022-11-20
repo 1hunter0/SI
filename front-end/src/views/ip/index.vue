264122f8814ff064924d43e13e25e23852c628e7
tanrenxuan <1057305176@qq.com>
2022-11-20 20:14:08 +0800

    feat: frontend add ip entity detail page - add subject alarm list - add object alarm list - add ip general description - fix some query bug

--- front-end/src/views/ip/index.vue
+++ front-end/src/views/ip/index.vue
@@ -69,7 +69,7 @@
       >
         <el-container class="top-container">
           <el-aside width="300px" class="logo">
-            <img :src="item.content.IpInfo.ip | logoFilter" style="margin-top:20px;" class="logo-content">
+            <img :src="item.content.IpInfo.ip | logoFilter" class="logo-content">
             <div class="logo-footer">
               <div class="risk">
                 <div class="title">风险评估</div>
@@ -81,13 +81,172 @@
               </div>
             </div>
           </el-aside>
-          <el-main>
-            Main
-          </el-main>
+          <el-container direction="vertical">
+            <el-main class="my-general-main">
+              <div class="name-tag">
+                <div class="name">
+                  <div class="title">IP</div>
+                  <span class="content">
+                    {{item.content.IpInfo.ip}}
+                  </span>
+                  <el-tooltip effect="dark" content="复制" placement="top"><i class="el-icon-copy-document copy" v-clipboard:copy="item.name" v-clipboard:success="onCopySuccess" v-clipboard:error="onCopyError"></i></el-tooltip>
+                </div>
+                <div class="tag">
+                  <span class="title">情报来源</span>
+                  <el-tooltip effect="dark" content="该实体情报的来源，可能是外生情报，也可能是从企业内部历史日志中搜集来" placement="top"><span class="my-icon"><i class="el-icon-zoom-in tag-tip"></i></span></el-tooltip>
+                  <div class="content">
+                    <el-tag v-for="(origin, index1) in tagFilter(item.content.IpInfo.ip)" :type="origin | tagClassFilter" :key="index1" effect="plain" size="mini">{{origin}}</el-tag>
+                  </div>
+                </div>
+              </div>
+            </el-main>
+            <el-main class="my-general-footer">
+              <div class="item-wrap">
+                <svg-icon icon-class="earth" />
+                <span class="label-wrap">地理位置</span>
+                <div class="value-wrap">{{item.content.IpInfo.country}}-{{item.content.IpInfo.province}}-{{item.content.IpInfo.city}}</div>
+              </div>
+              <div class="item-wrap">
+                <i class="el-icon-map-location"></i>
+                <span class="label-wrap">经纬度</span>
+                <div class="value-wrap">{{item.content.IpInfo.longitude}}-{{item.content.IpInfo.latitude}}</div>
+              </div>
+              <div class="item-wrap">
+                <svg-icon icon-class="sponer" />
+                <span class="label-wrap">运营商</span>
+                <div class="value-wrap">{{item.content.IpInfo.isp}}</div>
+              </div>
+            </el-main>
+          </el-container>
         </el-container>
-        <!-- <br/> -->
         <el-container class="bottom-container">
-            <el-main>Main</el-main>
+            <el-main>
+              <el-tabs v-model="item.activeName" type="card">
+                <el-tab-pane name="first">
+                  <span slot="label">
+                    <el-tooltip effect="dark" content="该ip作为主动方的所有告警日志" placement="top"><span>主动告警事件</span></el-tooltip>
+                  </span>
+                  <el-table
+                    :data="item.firstPage.dataList"
+                    fit
+                    border
+                    height="350"
+                    highlight-current-row
+                  >
+                    <el-table-column label="ip_subject" align="center" width="120" fixed>
+                      <template slot-scope="scope">
+                        {{ scope.row.ip_subject }}
+                      </template>
+                    </el-table-column>
+                    <el-table-column label="ip_object" align="center" width="120">
+                      <template slot-scope="scope">
+                        {{ scope.row.ip_object }}
+                      </template>
+                    </el-table-column>
+                    <el-table-column label="dev_info" align="center" width="150">
+                      <template slot-scope="scope">
+                        {{ scope.row.dev_info }}
+                      </template>
+                    </el-table-column>
+                    <el-table-column label="timestamp" align="center" width="180">
+                      <template slot-scope="scope">
+                        <i class="el-icon-time"></i>
+                        {{ scope.row.timestamp }}
+                      </template>
+                    </el-table-column>
+                    <el-table-column label="dev_category" align="center" width="150">
+                      <template slot-scope="scope">
+                        {{ scope.row.dev_category }}
+                      </template>
+                    </el-table-column>
+                    <el-table-column label="dev_rule" align="center" width="300">
+                      <template slot-scope="scope">
+                        {{ scope.row.dev_rule }}
+                      </template>
+                    </el-table-column>
+                    <el-table-column label="degree" align="center" width="80">
+                      <template slot-scope="scope">
+                        <el-tag :type="scope.row.degree | degreeClassFilter">{{ scope.row.degree}}</el-tag>
+                      </template>
+                    </el-table-column>
+                    <el-table-column label="hostname" align="center" width="150">
+                      <template slot-scope="scope">
+                        {{ scope.row.hostname }}
+                      </template>
+                    </el-table-column>
+                  </el-table>
+                  <el-pagination v-show="item.firstPage.total>0" :total="item.firstPage.total" :current-page="item.firstPage.page" :page-size="item.firstPage.limit" @current-change="fetchFirstData($event, item)" />
+
+                </el-tab-pane>
+                <el-tab-pane name="second">
+                  <span slot="label">
+                    <el-tooltip effect="dark" content="该ip作为被动方的所有告警日志" placement="top"><span>被动告警事件</span></el-tooltip>
+                  </span>
+                  <el-table
+                    :data="item.secondPage.dataList"
+                    fit
+                    border
+                    height="350"
+                    highlight-current-row
+                  >
+                    <el-table-column label="ip_object" align="center" width="120" fixed>
+                      <template slot-scope="scope">
+                        {{ scope.row.ip_object }}
+                      </template>
+                    </el-table-column>
+                    <el-table-column label="ip_subject" align="center" width="120">
+                      <template slot-scope="scope">
+                        {{ scope.row.ip_subject }}
+                      </template>
+                    </el-table-column>
+                    <el-table-column label="dev_info" align="center" width="150">
+                      <template slot-scope="scope">
+                        {{ scope.row.dev_info }}
+                      </template>
+                    </el-table-column>
+                    <el-table-column label="timestamp" align="center" width="180">
+                      <template slot-scope="scope">
+                        <i class="el-icon-time"></i>
+                        {{ scope.row.timestamp }}
+                      </template>
+                    </el-table-column>
+                    <el-table-column label="dev_category" align="center" width="150">
+                      <template slot-scope="scope">
+                        {{ scope.row.dev_category }}
+                      </template>
+                    </el-table-column>
+                    <el-table-column label="dev_rule" align="center" width="300">
+                      <template slot-scope="scope">
+                        {{ scope.row.dev_rule }}
+                      </template>
+                    </el-table-column>
+                    <el-table-column label="degree" align="center" width="80">
+                      <template slot-scope="scope">
+                        <el-tag :type="scope.row.degree | degreeClassFilter">{{ scope.row.degree}}</el-tag>
+                      </template>
+                    </el-table-column>
+                    <el-table-column label="hostname" align="center" width="150">
+                      <template slot-scope="scope">
+                        {{ scope.row.hostname }}
+                      </template>
+                    </el-table-column>
+                  </el-table>
+                  <el-pagination v-show="item.secondPage.total>0" :total="item.secondPage.total" :current-page="item.secondPage.page" :page-size="item.secondPage.limit" @current-change="fetchSecondData($event, item)" />
+                </el-tab-pane>
+                <el-tab-pane name="third">
+                  <span slot="label">
+                    <el-tooltip effect="dark" content="与此ip有关的所有样本" placement="top"><span>关联样本</span></el-tooltip>
+                  </span>
+                  与此ip有关的所有样本
+                </el-tab-pane>
+                <el-tab-pane name="forth">
+                  <span slot="label">
+                    <el-tooltip effect="dark" content="该ip的关联图" placement="top"><span>Graph</span></el-tooltip>
+                  </span>
+                  该ip的关联图
+                </el-tab-pane>
+              </el-tabs>
+            </el-main>
         </el-container>
         <!-- {{item.content}} -->
       </el-tab-pane>
@@ -109,7 +268,7 @@ export default {
         limit: 10,  // 每一页多少条记录
         total: 0,  // 总共有多少条记录
         param: {ip: ''},  // 可以根据ip地址模糊查询
-        temp_query: '', // 临时变量
+        temp_query: '', // 临时变量,否则点击跳转页面会根据当前搜索框内容查询，出现bug
       },
       tabs: { // 标签页数据
         activeName: 'ipListPage',
@@ -134,7 +293,7 @@ export default {
       }
       return levelToLogo[level]
     },
-    levelFilter(ip) {
+    levelFilter(ip) { // todo(tanrenxuan): replace with our field
       let level = ip.length % 2 // 0: 安全 1：危险
       const levelToDescription = {
         0: "安全",
@@ -142,7 +301,7 @@ export default {
       }
       return levelToDescription[level]
     },
-    levelClassFilter(ip) {
+    levelClassFilter(ip) { // todo(tanrenxuan): replace with our field
       let level = ip.length % 2 // 0: 安全 1：危险
       const levelToClass = {
         0: "safe",
@@ -150,7 +309,7 @@ export default {
       }
       return [levelToClass[level],'value']
     },
-    confidenceFilter(ip) {
+    confidenceFilter(ip) { // todo(tanrenxuan): replace with our field
       let confidence = ip.length % 3 // 0: 低 1:中 2:高
       const confidenceToDescription = {
         0: "低",
@@ -158,7 +317,24 @@ export default {
         2: "高"
       }
       return confidenceToDescription[confidence]
-    }
+    },
+    tagClassFilter(origin) { // todo(tanrenxuan): replace with our field
+      const originToTag = {
+        '蜜罐': 'info',
+        '日志易-waf': 'info',
+        '日志易-云澈': '',
+        '外生': 'success',
+      }
+      return originToTag[origin]
+    },
+    degreeClassFilter(degree) {
+      const degreeToClass = {
+        'low': "success",
+        'middle': "warning",
+        'high': "danger",
+      }
+      return degreeToClass[degree]
+    },
   },
   mounted() {
     this.fetchData()
@@ -207,6 +383,25 @@ export default {
       this.tabs.detailTabs.push({
         title: ip + '详情',
         name: ip,
+        activeName: 'first',
+        firstPage: {
+          page: 1,  // 默认是第一页
+          limit: 5,  // 每一页多少条记录
+          total: 0,  // 总共有多少条记录
+          dataList: []
+        },
+        secondPage: {
+          page: 1,  // 默认是第一页
+          limit: 5,  // 每一页多少条记录
+          total: 0,  // 总共有多少条记录
+          dataList: []
+        },
+        thirdPage: {
+          page: 1,  // 默认是第一页
+          limit: 5,  // 每一页多少条记录
+          total: 0,  // 总共有多少条记录
+          dataList: []
+        },
         content: {
           IpInfo: {
             ip: '',
@@ -227,13 +422,36 @@ export default {
       this.$nextTick(function() {
         let loading = this.startLoading(document.getElementById(ip))
         getIpInfo(ip).then(response => {
-          this.tabs.detailTabs[this.tabs.detailTabs.length-1].content = response.Data
+          let currTab = this.tabs.detailTabs[this.tabs.detailTabs.length-1]
+          currTab.content = response.Data
+          currTab.firstPage.total = currTab.content.Alarms.subject_alarms.length
+          currTab.firstPage.dataList = this.fetchDataFromTab(currTab.firstPage, currTab.content.Alarms.subject_alarms)
+          currTab.secondPage.total = currTab.content.Alarms.object_alarms.length
+          currTab.secondPage.dataList = this.fetchDataFromTab(currTab.secondPage, currTab.content.Alarms.object_alarms)
           loading.close()
         }).catch(err => {
           // nothing to do...
           console.log(err)
         })
       })
+    },
+
+    fetchDataFromTab(page, items) {
+      let curr_page = page.page
+      let page_size = page.limit
+      let startNumber = (curr_page-1)*page_size
+      let endNumber = (startNumber + page_size > items.length) ? items.length : startNumber + page_size
+      return items.slice(startNumber, endNumber)
+    },
+
+    fetchFirstData(val, currTab) {
+      currTab.firstPage.page = val
+      currTab.firstPage.dataList = this.fetchDataFromTab(currTab.firstPage, currTab.content.Alarms.subject_alarms)
+    },
+
+    fetchSecondData(val, currTab){
+      currTab.secondPage.page = val
+      currTab.secondPage.dataList = this.fetchDataFromTab(currTab.secondPage, currTab.content.Alarms.object_alarms)
     },
 
     removeTab(targetName) {
@@ -255,6 +473,19 @@ export default {
       this.tabs.activeName =  currActiveName
       this.tabs.detailTabs = tabList.filter(tab => tab.name !== targetName)
     },
+
+    onCopySuccess() {
+      this.$message.success("复制成功");
+    },
+    onCopyError() {
+      this.$message.error("复制失败");
+    },
+
+    tagFilter(ip) {
+      const originLst = ['蜜罐', '日志易-waf', '日志易-云澈', '外生']
+      let index = ip.length % 4
+      return [originLst[index], originLst[(index + 2) % 4]]
+    },
   },
   activated() {
     if(this.$route.query.ip) {
@@ -268,23 +499,20 @@ export default {
 <style scoped>
 
   .el-aside {
-    /* background-color: #D3DCE6; */
+    /* background-color: #E9EEF3; */
     color: #333;
     text-align: center;
   }
 
+
   .el-main {
-    background-color: #E9EEF3;
+    /* background-color: #E9EEF3; */
     color: #333;
-    text-align: center;
-    line-height: 160px;
   }
 
   .my-main-container {
     border: 1px solid #e2e4e6;
   }
-
-
 
   .top-container {
     margin-bottom: 20px;
@@ -292,12 +520,16 @@ export default {
   }
 
   .bottom-container {
-    height: 500px;
+    height: 450px;
   }
 
 
   .logo {
     text-align: center;
+  }
+
+  .logo .logo-content {
+    margin-top:20px;
   }
 
   .logo .logo-footer {
@@ -333,5 +565,86 @@ export default {
   .logo .logo-footer .risk .danger {
     color: #c41a1a;
   }
+
+  .my-general-main {
+    height: 200px;
+    border-bottom: 1px solid #e2e4e6;
+  }
+
+  .my-general-main .name-tag {
+    text-align: left;
+    margin-top: 10px
+  }
+
+  .my-general-main .name-tag .name {
+    padding-bottom: 40px;
+    width: 100%;
+  }
+
+  .my-general-main .name-tag .name .title {
+    width: 100%;
+    font-size: 14px;
+    color: #969799;
+  }
+
+  .my-general-main .name-tag .name .content {
+    font-size: 32px;
+    line-height: 35px;
+    font-weight: 600;
+    color: #2b2c2d;
+    float: left;
+    text-overflow: ellipsis;
+    overflow: hidden;
+    white-space: nowrap;
+  }
+
+  .my-general-main .name-tag .name .copy {
+    margin-left: 10px;
+  }
+
+  .my-general-main .name-tag .tag .title {
+    color: #969799;
+    font-size: 14px;
+    line-height: 25px;
+  }
+
+  .my-general-main .name-tag .tag .my-icon {
+    font-size: 10px;
+    margin-left: 2px;
+  }
+
+  .my-general-footer {
+    height: 100px
+  }
+
+  .my-general-footer .item-wrap {
+    float: left;
+    width: 33.333333%;
+    min-height: 60px;
+    padding: 2px 8px 0;
+    height: 50%
+  }
+
+  .my-general-footer .item-wrap .icon-wrap {
+    line-height: 35px;
+    color: #2b2c2d;
+  }
+
+  .my-general-footer .item-wrap .label-wrap {
+      font-size: 14px;
+      color: #2b2c2d;
+      margin-left: 4px
+  }
+
+  .my-general-footer .item-wrap .value-wrap {
+    font-size: 12px;
+    line-height: 20px;
+    color: #646566;
+    word-break: break-all;
+  }
+
+
+
+
 
 </style>
